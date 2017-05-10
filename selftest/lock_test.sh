a6278b7fd0887da753ca5787cce3dc239fac8c71
Neels Hofmeyr <neels@hofmeyr.de>
2017-05-10 13:46:11 +0200

    cosmetic: lock test: add quotes
    Mostly to clarify an empty lock in the end.
    
    Change-Id: If80960b331d30125e97878d52edba9b4a63e87c7

--- selftest/lock_test.sh
+++ selftest/lock_test.sh
@@ -11,17 +11,17 @@ python3 ./lock_test_help.py "$dir" "$n1" &
 while [ ! -f "$dir/lock_test" ]; do
   sleep .1
 done
-echo "launched first, locked by: $(cat "$dir/lock_test")"
+echo "launched first, locked by: '$(cat "$dir/lock_test")'"
 python3 ./lock_test_help.py "$dir" "$n2" &
-echo "launched second, locked by: $(cat "$dir/lock_test")"
+echo "launched second, locked by: '$(cat "$dir/lock_test")'"
 rm "$f1"
 while [ ! -f "$f1.done" ]; do
   sleep .1
 done
-echo "waited, locked by: $(cat "$dir/lock_test")"
+echo "waited, locked by: '$(cat "$dir/lock_test")'"
 rm "$f2"
 while [ ! -f "$f2.done" ]; do
   sleep .1
 done
-echo "waited more, locked by: $(cat "$dir/lock_test")"
+echo "waited more, locked by: '$(cat "$dir/lock_test")'"
 rm -rf "$dir"

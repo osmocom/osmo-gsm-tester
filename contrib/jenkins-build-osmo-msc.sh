db2d76bb52a360a975e36f054af98efd9b0d2ee5
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2017-06-29 19:45:45 +0200

    jenkins: build msc using libosmo-sccp master
    Since 81c0bcab068174036f6897e69311f413d94c74d4 was merged, it is not necessary
    to build the neels/aoip branch anymore. Other patches on that branch are not
    necessary either.
    
    Change-Id: I850e0dd2fc61d59d56c0dd69ee00b6e19685fb95

--- contrib/jenkins-build-osmo-msc.sh
+++ contrib/jenkins-build-osmo-msc.sh
@@ -9,7 +9,7 @@ build_repo libosmo-abis
 build_repo libosmo-netif
 build_repo openggsn
 build_repo libsmpp34
-build_repo libosmo-sccp neels/aoip # TEMPORARY BRANCH
+build_repo libosmo-sccp
 build_repo libasn1c
 build_repo osmo-iuh neels/sigtran # TEMPORARY BRANCH
 build_repo openbsc aoip --enable-smpp --enable-osmo-bsc --enable-nat --enable-iu

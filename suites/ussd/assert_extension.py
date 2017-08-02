2d9c14aeb93ad8158a4657441a67fe8cb3c1d7a5
Pau Espin Pedrol <pespin@sysmocom.de>
2017-08-02 13:08:35 +0200

    ussd: Workaround ofono issue to prevent test failure
    Change-Id: I72f68bc980e6421a65f7d33712a587da340698e5

--- suites/ussd/assert_extension.py
+++ suites/ussd/assert_extension.py
@@ -21,6 +21,16 @@ print('waiting for modems to attach...')
 wait(ms.is_connected, nitb.mcc_mnc())
 wait(nitb.subscriber_attached, ms)
 
+# ofono (qmi) currently changes state to 'registered' jut after sending
+# 'Location Update Request', but before receiving 'Location Updating Accept'.
+# Which means we can reach lines below and send USSD code while still not being
+# attached, which will then fail. See OsmoGsmTester #2239 for more detailed
+# information.
+# Until we find an ofono fix or a better way to workaround this, let's just
+# sleep for a while in order to receive the 'Location Updating Accept' message
+# before attemting to send the USSD.
+sleep(10)
+
 print('Sending ussd code %s' % USSD_COMMAND_GET_EXTENSION)
 response = ms.ussd_send(USSD_COMMAND_GET_EXTENSION)
 assert ' ' + ms.msisdn + '\r' in response

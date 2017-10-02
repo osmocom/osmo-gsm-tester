e17d13dfabef6736b1de2e6a7dd7c53d18c8651f
Neels Hofmeyr <neels@hofmeyr.de>
2017-10-02 16:12:25 +0200

    jenkins: fix build of osmo-sgsn: use osmo-ggsn
    osmo-sgsn change-id I91a126952ae75ae9ba1f42aac4d26413384bba5d and commit
    a3b73e917b653d13d027cff2c95a56bbc1264446 has broken compatibility with
    openggsn.git, by requiring current osmo-ggsn version (1.0.0).
    
    Fix build fallout: use osmo-ggsn instead of openggsn.git.
    
    Change-Id: I5161983ef625465e21b95578ab5648ecc73d6069

--- contrib/jenkins-build-osmo-sgsn.sh
+++ contrib/jenkins-build-osmo-sgsn.sh
@@ -8,7 +8,7 @@ build_repo libosmocore --disable-doxygen
 build_repo libosmo-abis
 build_repo libosmo-netif --disable-doxygen
 build_repo libosmo-sccp
-build_repo openggsn
+build_repo osmo-ggsn
 build_repo osmo-sgsn --disable-iu
 
 create_bin_tgz osmo-sgsn

9695a1cbc13fef0afe4ecdb9c098193aab423cdf
Alexander Couzens <lynxis@fe80.eu>
2018-03-06 14:31:26 +0100

    jenkins: use env POKY_VERSION POKY_PATH to find poky
    Instead of hardcoded values, use defaults and allow jenkins to override
    the defaults.
    
    Change-Id: I1403b32cdf30420f3b02c41c2627b71fcbeae9ab

--- contrib/jenkins-build-osmo-pcu-sysmo.sh
+++ contrib/jenkins-build-osmo-pcu-sysmo.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e -x
 
-poky="/opt/poky/1.5.4"
-. "$poky/environment-setup-armv5te-poky-linux-gnueabi"
+_poky_version="$POKY_VERSION"
+_poky_path="$POKY_PATH"
+
+[ -z "$_poky_version" ] && _poky_version="1.5.4"
+[ -z "$_poky_path" ] && _poky_path="/opt/poky/$_poky_version"
+
+. "$_poky_path/environment-setup-armv5te-poky-linux-gnueabi"
 
 # Cross-compilation: all installations need to be put in the sysmo SDK sysroot
-export DESTDIR="$poky/sysroots/armv5te-poky-linux-gnueabi"
+export DESTDIR="$_poky_path/sysroots/armv5te-poky-linux-gnueabi"
 
 base="$PWD"
 name="osmo-pcu-sysmo"

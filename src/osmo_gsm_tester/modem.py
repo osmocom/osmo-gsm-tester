f8d12196f05b20aa28b3103db26ea37ba6849362
Pau Espin Pedrol <pespin@sysmocom.de>
2018-03-14 19:19:28 +0100

    modem: workaround ofono crash
    Since commit bfd0b2310cf09e32cb7d5dbe74ec57606a7d2aab, the IMSI is
    retreived from ofono. To get the IMSI, the modme must be powered ON
    previously. The imsi() method is called before calling connect() in
    order to set up the IMSI in the HLR. As a result, the following
    behaviour occurs:
    - Powered=true (in imsi())
    - SimManager.GetProperties() (several of them)
    - Powered=false (power_cycle() in connect()
    
    It seems powering off the modem immediately after using the SIM service
    makes ofono crash. Proof of it is that crashes were seen mainly on tests
    using only 1 MS, in which we don't spend that much time between
    SimManager.GetProperties() and Powered=false for the same modem.
    
    Let's workaround the crash for now increasing the time between using the
    SIM services and powering the modem off.
    
    Related: OS#3064
    
    Change-Id: Ief052cac5a862d6ef9391d40c294ba017387506c

--- src/osmo_gsm_tester/modem.py
+++ src/osmo_gsm_tester/modem.py
@@ -577,6 +577,7 @@ class Modem(log.Origin):
         req_ifaces = self._required_ifaces()
         if self.is_powered():
             self.dbg('Power cycling')
+            event_loop.sleep(self, 1.0) # workaround for ofono bug OS#3064
             self.power_off()
         else:
             self.dbg('Powering on')

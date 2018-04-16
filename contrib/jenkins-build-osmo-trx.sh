ec3852f1bd12d61998aabbcf44b8056946542191
Pau Espin Pedrol <pespin@sysmocom.de>
2018-04-16 14:22:50 +0200

    contrib: build-osmo-trx: Enable addr sanitize for osmo-trx
    Change-Id: Iad90c7c8c2098680276de000c36bec0224f9746d

--- contrib/jenkins-build-osmo-trx.sh
+++ contrib/jenkins-build-osmo-trx.sh
@@ -5,6 +5,6 @@ name="osmo-trx"
 . "$(dirname "$0")/jenkins-build-common.sh"
 
 build_repo libosmocore --enable-sanitize --disable-doxygen
-build_repo osmo-trx --without-sse
+build_repo osmo-trx --enable-sanitize --without-sse
 
 create_bin_tgz osmo-trx

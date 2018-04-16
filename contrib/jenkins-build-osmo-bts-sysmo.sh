4a338c7dbac31133406154d1e27faea1dc25f291
Pau Espin Pedrol <pespin@sysmocom.de>
2018-04-16 13:57:49 +0200

    contrib: build-osm-bts-sysmo: openbsc dep no longer needed
    Change-Id: Iafd5ee1df001719ff6165ada996359b92116671e

--- contrib/jenkins-build-osmo-bts-sysmo.sh
+++ contrib/jenkins-build-osmo-bts-sysmo.sh
@@ -18,9 +18,6 @@ prefix="/usr/local/jenkins-build/inst-$name"
 prefix_real="$DESTDIR$prefix"
 . "$(dirname "$0")/jenkins-build-common.sh"
 
-# for gsm_data_shared.h
-have_repo openbsc
-
 build_repo libosmocore --disable-pcsc --disable-doxygen --disable-gnutls
 build_repo libosmo-abis
 build_repo osmo-bts --enable-sysmocom-bts --with-openbsc=$base/openbsc/openbsc/include

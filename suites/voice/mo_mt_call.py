3bf1e13b13d499457f6f17f7c41622870ca5fcc6
Pau Espin Pedrol <pespin@sysmocom.de>
2018-05-08 15:59:26 +0200

    tests: voice: Move mo_mt_call.py to mo_mt_call_tchf.py
    Change-Id: I7306e62ad4c4294370690cf9006871f798a7b9ec

--- suites/voice/mo_mt_call.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from osmo_gsm_tester.testenv import *
-from testlib import call_test_setup_run
-
-call_test_setup_run()
---
+++ suites/voice/mo_mt_call_tchf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from osmo_gsm_tester.testenv import *
+
+add_subdir_to_import_path("lib")
+from testlib import call_test_setup_run
+
+def my_bts_setup(bts):
+    bts.set_num_trx(1)
+    bts.set_trx_phy_channel(0, 0, 'CCCH+SDCCH4')
+    bts.set_trx_phy_channel(0, 1, 'SDCCH8')
+    bts.set_trx_phy_channel(0, 2, 'TCH/F')
+    bts.set_trx_phy_channel(0, 3, 'TCH/F')
+    bts.set_trx_phy_channel(0, 4, 'TCH/F')
+    bts.set_trx_phy_channel(0, 5, 'TCH/F')
+    bts.set_trx_phy_channel(0, 6, 'TCH/F')
+    bts.set_trx_phy_channel(0, 7, 'TCH/F')
+
+call_test_setup_run(bts_setup_cb=my_bts_setup)

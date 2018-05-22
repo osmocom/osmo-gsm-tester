618bb3a6bf066efa5430f185a4a6659274cbae67
Pau Espin Pedrol <pespin@sysmocom.de>
2018-05-22 15:15:39 +0200

    contrib: build-osmocom-bb: Download and install osmocom-bb fw from OBS
    As it's quite complex to build osmocom firmware nowadays (a specific old
    release of gcc and binutils are required to build it), instead download
    the already built binaries from OBS.
    
    Change-Id: I2571268d91e2430a270e51438608e7dbd121e76d

--- contrib/jenkins-build-osmocom-bb.sh
+++ contrib/jenkins-build-osmocom-bb.sh
@@ -5,6 +5,8 @@ base="$PWD"
 name="osmocom-bb"
 . "$(dirname "$0")/jenkins-build-common.sh"
 
+FW_RPM_URL="http://download.opensuse.org/repositories/home:/mnhauke:/osmocom:/nightly/SLE_15/x86_64/"
+
 build_repo libosmocore --disable-doxygen
 
 have_repo osmocom-bb
@@ -20,5 +22,12 @@ make -j4
 set +x; echo; echo; set -x
 make install
 
+mkdir -p "$prefix"
+cd "$prefix"
+FW_RPM="$(wget -q -O - "$FW_RPM_URL" | grep -o 'osmocom-bb-firmware.*rpm' | sed 's#\"#\n#g' | head -1)"
+echo "Downloading RPM package $FW_RPM"
+wget -q "$FW_RPM_URL/$FW_RPM" -O osmocom-bb-firmware.rpm
+rpm2cpio osmocom-bb-firmware.rpm  | cpio -idmv
+rm osmocom-bb-firmware.rpm
 
 create_bin_tgz "" "osmocon"

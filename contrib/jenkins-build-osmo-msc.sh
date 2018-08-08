9d8fec54e6910bc7b05360bbfae622fb24d83aa6
Harald Welte <laforge@gnumonks.org>
2018-08-08 08:01:47 +0200

    jenkins-build-osmo-msc.sh: Build osmo-hlr before osmo-msc
    osmo-msc now needs libosmo-gsup-client, which is built during omso-hlr
    compilation.
    
    Change-Id: I67c5d168011a385fd4f68df351e0a93e9bef37a7

--- contrib/jenkins-build-osmo-msc.sh
+++ contrib/jenkins-build-osmo-msc.sh
@@ -10,6 +10,7 @@ build_repo libosmo-netif --enable-sanitize --disable-doxygen
 build_repo libsmpp34 --enable-sanitize
 build_repo libosmo-sccp --enable-sanitize
 build_repo osmo-mgw --enable-sanitize
+build_repo osmo-hlr --enable-sanitize
 build_repo osmo-msc --enable-sanitize --enable-smpp --disable-iu
 
 create_bin_tgz osmo-msc

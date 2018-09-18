fd760f6702340fa79a971485d9c683ad7ca8db03
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2018-09-18 08:02:21 +0100

    Package mobile and virtphy as part of osmocom-bb as well
    Change-Id: Ie44dca72327e928073b00cde434d1cf2de574d9c

--- contrib/jenkins-build-osmocom-bb.sh
+++ contrib/jenkins-build-osmocom-bb.sh
@@ -8,19 +8,9 @@ name="osmocom-bb"
 FW_RPM_URL="http://download.opensuse.org/repositories/home:/mnhauke:/osmocom:/nightly/SLE_15/x86_64/"
 
 build_repo libosmocore --disable-doxygen
-
-have_repo osmocom-bb
-cd osmocom-bb/
-
-cd src/host/osmocon/
-set +x; echo; echo; set -x
-autoreconf -fi
-set +x; echo; echo; set -x
-./configure --prefix="$prefix" $CONFIGURE_FLAGS $configure_opts
-set +x; echo; echo; set -x
-make -j4
-set +x; echo; echo; set -x
-make install
+build_repo_dir osmocom-bb src/host/virt_phy --enable-sanitize
+build_repo_dir osmocom-bb src/host/osmocon --enable-sanitize
+build_repo_dir osmocom-bb src/host/layer23 --enable-sanitize
 
 mkdir -p "$prefix"
 cd "$prefix"
@@ -30,4 +20,4 @@ wget -q "$FW_RPM_URL/$FW_RPM" -O osmocom-bb-firmware.rpm
 rpm2cpio osmocom-bb-firmware.rpm  | cpio -idmv
 rm osmocom-bb-firmware.rpm
 
-create_bin_tgz "" "osmocon"
+create_bin_tgz "virtphy mobile" "osmocon"

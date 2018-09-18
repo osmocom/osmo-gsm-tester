d301bf9f84046169a714e86d8efb77451bd3c50b
Pau Espin Pedrol <pespin@sysmocom.de>
2018-09-18 12:49:47 +0200

    suites: encryption: Add test register_a5_3_authreq
    Change-Id: I279e78e741b3ca841cb8508db40874f3c17b0785

---
+++ suites/encryption/register_a5_3_authreq.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from osmo_gsm_tester.testenv import *
+
+import testlib
+suite.test_import_modules_register_for_cleanup(testlib)
+from testlib import encryption_test_setup_run
+
+encryption_test_setup_run(True, 'a5_3')

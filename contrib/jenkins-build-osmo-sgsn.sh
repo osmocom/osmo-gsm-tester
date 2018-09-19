8cc84d84046063818f01f8aaad808b7b551385cf
Pau Espin Pedrol <pespin@sysmocom.de>
2018-09-19 14:05:41 +0200

    contrib: Build osmo-sgsn with IU support
    Change-Id: I810ae848c350550430c1918b3abb4376d21d7317

--- contrib/jenkins-build-osmo-sgsn.sh
+++ contrib/jenkins-build-osmo-sgsn.sh
@@ -9,6 +9,8 @@ build_repo libosmo-abis --enable-sanitize
 build_repo libosmo-netif --enable-sanitize --disable-doxygen
 build_repo libosmo-sccp --enable-sanitize
 build_repo osmo-ggsn --enable-sanitize
-build_repo osmo-sgsn --enable-sanitize --disable-iu
+build_repo libasn1c --enable-sanitize
+build_repo osmo-iuh --enable-sanitize
+build_repo osmo-sgsn --enable-sanitize --enable-iu
 
 create_bin_tgz osmo-sgsn

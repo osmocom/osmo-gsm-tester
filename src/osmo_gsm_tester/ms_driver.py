51ae2b5f5ce2ed190920d44b994b7027cda4c64a
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2018-12-02 15:52:17 +0000

    Fix and use the right member variable
    Change-Id: Ib5d50322ecd30c7724eef8452d22845450e9c633

--- src/osmo_gsm_tester/ms_driver.py
+++ src/osmo_gsm_tester/ms_driver.py
@@ -82,7 +82,7 @@ class MsDriver(log.Origin):
         """
 
         # Get the base directory for the virtphy/mobile application
-        inst = util.Dir(os.path.abspath(self.suite_run.trial.get_inst('osmocom-bb')))
+        inst = util.Dir(os.path.abspath(self._suite_run.trial.get_inst('osmocom-bb')))
 
         # Assume these are dynamically linked and verify there is a lib dir.
         lib = inst.child('lib')
@@ -113,7 +113,7 @@ class MsDriver(log.Origin):
         options = self.build_binary_options()
         self._test_case = MassUpdateLocationTest("mass", options, self._num_ms, self._cdf,
                                                  self._ev_server,
-                                                 util.Dir(self.suite_run.get_test_run_dir()),
+                                                 util.Dir(self._suite_run.get_test_run_dir()),
                                                  suite_run=self._suite_run)
 
         # TODO: We should pass subscribers down to the test and not get it from

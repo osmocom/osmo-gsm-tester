4db8bb68c11a320bafc70d751f9ea6a3f1bbd1c4
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2018-12-15 14:37:00 +0000

    osmo-bts: Package the osmo-bts-virtual binary for the ms_driver
    Change-Id: Id6db5d0bea5262075c3e06fbb0ccfef86afa85e4

--- contrib/jenkins-build-osmo-bts.sh
+++ contrib/jenkins-build-osmo-bts.sh
@@ -10,4 +10,4 @@ build_repo libosmocore ${SANITIZE_FLAGS} --disable-doxygen
 build_repo libosmo-abis ${SANITIZE_FLAGS}
 build_repo osmo-bts ${SANITIZE_FLAGS} --enable-trx --with-openbsc=$base/openbsc/openbsc/include --enable-octphy --with-octsdr-2g=$base/octphy-2g-headers
 
-create_bin_tgz "osmo-bts-trx osmo-bts-octphy"
+create_bin_tgz "osmo-bts-trx osmo-bts-octphy osmo-bts-virtual"

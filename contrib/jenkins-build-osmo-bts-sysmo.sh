bf3bb49785e07e87f12d7af2a069d56a9ae4a97a
Pau Espin Pedrol <pespin@sysmocom.de>
2019-08-06 16:04:58 +0200

    contrib: Avoid sysmobts/oc2g toolchain ld crashes building new libosmocore
    Some gcc version (< 7.3.0) on ARM generate wrong code for __thread
    variables that crash at runtime. It seems, due to some unknown reason, that we
    are not affected by this bug with toolchain from poky pyro (6.4.0). However,
    libosmocore as of e188b8cd98f599468fbb200c7d590de955daf761 applies some
    CFLAGS="-mtls-dialect=gnu2" to workaround the issue automatically. But same
    toolchain contains an "ld" that crashes when those flags are applied.
    Fortunately, libosmocore provides a way to disable the workaround, and since
    we are not affected by the bug, we are fine with that. If the toolchain had
    the TLS runtime bug, then we'd had to build with -O0. Can be probably dropped
    once we move to newer toolchain.
    
    Depends: libosmocore.git afce89dac1a3cdb975da72020acdba16940fd679
    Related: OS#4062
    Change-Id: I583354735a7671e5bba1c0f6e60c84b6ef55ac63

--- contrib/jenkins-build-osmo-bts-sysmo.sh
+++ contrib/jenkins-build-osmo-bts-sysmo.sh
@@ -18,7 +18,7 @@ prefix="/usr/local/jenkins-build/inst-$name"
 prefix_real="$DESTDIR$prefix"
 . "$(dirname "$0")/jenkins-build-common.sh"
 
-build_repo libosmocore --disable-pcsc --disable-doxygen --disable-gnutls
+build_repo libosmocore --disable-pcsc --disable-doxygen --disable-gnutls --disable-detect-tls-gcc-arm-bug
 build_repo libosmo-abis
 build_repo osmo-bts --enable-sysmocom-bts --with-openbsc=$base/openbsc/openbsc/include
 

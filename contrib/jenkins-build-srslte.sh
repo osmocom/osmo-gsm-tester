1bce944fa26fccbeeda9ed82f632e0c02781ff36
Pau Espin Pedrol <pespin@sysmocom.de>
2020-02-11 14:24:53 +0100

    jenkins-build-srslte: Temporarily Switch git repo to own-controlled one
    During integration of the different srsLTE processes, some bugs or
    misbehaviors appear which require fixes in srsLTE. Let's use our own
    temporary fork until integration is complete and all patches are merged
    in upstream.
    
    Change-Id: I6414d9855cf9c004385cc280e5a60e82f8e4fbad

--- contrib/jenkins-build-srslte.sh
+++ contrib/jenkins-build-srslte.sh
@@ -3,7 +3,7 @@ set -e -x
 
 base="$PWD"
 name="srslte"
-git_url="https://github.com/srsLTE/"
+git_url="${git_url-"https://github.com/pespin/"}"
 . "$(dirname "$0")/jenkins-build-common.sh"
 
 #TODO: make sure libconfig, zeroMQ is installed

979f00d43da2476e5110c3098cca67a0c8fbcea8
Pau Espin Pedrol <pespin@sysmocom.de>
2020-02-17 17:56:47 +0100

    contrib: srslte: Allow changing repo name
    Change-Id: I06e6566ac3df9369b391bc88f288f72588088ef1

--- contrib/jenkins-build-srslte.sh
+++ contrib/jenkins-build-srslte.sh
@@ -4,9 +4,10 @@ set -e -x
 base="$PWD"
 name="srslte"
 git_url="${git_url:-https://github.com/srsLTE}"
+project_name="${project_name:-srsLTE}"
 . "$(dirname "$0")/jenkins-build-common.sh"
 
 #TODO: make sure libconfig, zeroMQ is installed
-build_repo srsLTE
+build_repo $project_name
 
 create_bin_tgz "srsue srsenb srsepc"

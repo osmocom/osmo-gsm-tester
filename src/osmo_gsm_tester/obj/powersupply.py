osmocom/osmo-gsm-tester

commit 2b959580b9affe729e966c506656b7f47926c4bf

powersupply: Import sublcass module only if used
This way we avoid unconditionally importing all subclass dependencies,
and make them optional based on whether the setup has devices of that
type or not.

Change-Id: I0d2f8b26364b45f0d837cc51078784d1d0fa7ea1

src/osmo_gsm_tester/obj/powersupply.py
@@ -50,23 +50,17 @@ class PowerSupply(log.Origin, metaclass=ABCMeta):
         MainLoop.sleep(self, sleep)
         self.power_set(True)
 
-
-from . import powersupply_sispm, powersupply_intellinet
-
-KNOWN_PWSUPPLY_TYPES = {
-        'sispm' : powersupply_sispm.PowerSupplySispm,
-        'intellinet' : powersupply_intellinet.PowerSupplyIntellinet,
-}
-
-def register_type(name, clazz):
-    """Register a new PoerSupply child class at runtime."""
-    KNOWN_PWSUPPLY_TYPES[name] = clazz
-
 def get_instance_by_type(pwsupply_type, pwsupply_opt):
     """Allocate a PowerSupply child class based on type. Opts are passed to the newly created object."""
-    obj = KNOWN_PWSUPPLY_TYPES.get(pwsupply_type, None)
-    if not obj:
+    if pwsupply_type == 'sispm':
+        from powersupply_sispm import PowerSupplySispm
+        obj = PowerSupplySispm
+    elif pwsupply_type == 'intellinet':
+        from powersupply_intellinet import PowerSupplyIntellinet
+        obj = PowerSupplyIntellinet
+    else:
         raise log.Error('PowerSupply type not supported:', pwsupply_type)
+
     return obj(pwsupply_opt)
 
 

commit a65b505adc782533122589abab2c63538f98dd49

powersupply.py: Fix import module line typo
Recent commit changed the import line so it runs only when required, but
added a typo making tests uing it fail.

Change-Id: Id3e5c16adc763a9b79cc464937c541abd15f98de
Fixes: 2b959580b9affe729e966c506656b7f47926c4bf

src/osmo_gsm_tester/obj/powersupply.py
@@ -53,10 +53,10 @@ class PowerSupply(log.Origin, metaclass=ABCMeta):
 def get_instance_by_type(pwsupply_type, pwsupply_opt):
     """Allocate a PowerSupply child class based on type. Opts are passed to the newly created object."""
     if pwsupply_type == 'sispm':
-        from powersupply_sispm import PowerSupplySispm
+        from .powersupply_sispm import PowerSupplySispm
         obj = PowerSupplySispm
     elif pwsupply_type == 'intellinet':
-        from powersupply_intellinet import PowerSupplyIntellinet
+        from .powersupply_intellinet import PowerSupplyIntellinet
         obj = PowerSupplyIntellinet
     else:
         raise log.Error('PowerSupply type not supported:', pwsupply_type)
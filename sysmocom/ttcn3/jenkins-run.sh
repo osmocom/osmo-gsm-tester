32f3e169b3f770e3a040ce73bf6e1af2ccc473d6
Pau Espin Pedrol <pespin@sysmocom.de>
2020-06-09 16:22:43 +0200

    sysmocom/ttcn3/jenkins-run.sh: Pass confg file as cmdline
    Fix after recent huge refactoring.
    
    Change-Id: If0bcc9f7cb70e4ef324b3295d2174d11982a4bde

--- sysmocom/ttcn3/jenkins-run.sh
+++ sysmocom/ttcn3/jenkins-run.sh
@@ -44,7 +44,7 @@ rm *.md5
 # jenkins build job.
 # On failure, first clean up below and then return the exit code.
 exit_code="1"
-if python3 -u "$(which osmo-gsm-tester.py)" "$trial_dir" $OSMO_GSM_TESTER_OPTS ; then
+if python3 -u "$(which osmo-gsm-tester.py)" -c "$OSMO_GSM_TESTER_CONF" "$trial_dir" $OSMO_GSM_TESTER_OPTS ; then
   exit_code="0"
 fi
 

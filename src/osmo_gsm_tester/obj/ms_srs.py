c487f7438c887955f348864a1e2663b1a3c469cf
Andre Puschmann <andre@softwareradiosystems.com>
2021-02-23 11:47:53 +0100

    ms_srs: add config scheme to select NAS log level
    we've provided only all_log_level so far but sometimes it's needed
    to select the level per layer. This patch adds the ability
    to do so for the NAS layer in the UE.
    
    Change-Id: Iab2bce65e8af81f6d344849c97952e6441cb2846

--- src/osmo_gsm_tester/obj/ms_srs.py
+++ src/osmo_gsm_tester/obj/ms_srs.py
@@ -50,6 +50,7 @@ def on_register_schemas():
     config_schema = {
         'enable_pcap': schema.BOOL_STR,
         'log_all_level': schema.STR,
+        'log_nas_level': schema.STR
         }
     schema.register_config_schema('modem', config_schema)
 

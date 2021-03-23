26ee595e1445253ecd66b1f746ae570b132eb556
AlaiaL <alejandro.leal@softwareradiosystems.com>
2021-03-23 12:33:08 +0100

    jenkins-build-amarisoft: adjusting lib name to srsran and update versions
    fix rename of srslte to srsran libs and also update Amarisoft
    versions.
    
    Change-Id: I2174aec3c7ded8c966877a87f4cc6a39ce1325c2

--- contrib/jenkins-build-amarisoft.sh
+++ contrib/jenkins-build-amarisoft.sh
@@ -36,23 +36,29 @@ fi
 rm -rf inst-tmp && mkdir inst-tmp
 rm -rf inst-tmp-uhd && mkdir inst-tmp-uhd
 tar -zxf $amarisoft_tgz -C inst-tmp/
-tar -zxf inst-tmp/*/trx_uhd*.tar.gz -C inst-tmp/
+for file in inst-tmp/*/trx_uhd*.tar.gz; do tar -zxf "$file" -C inst-tmp/; done
 
 # Build trx_uhd.so:
-cd ${base}/inst-tmp/trx_uhd-linux*/
-make
-cd ${base}
+for path in ${base}/inst-tmp/trx_uhd-linux*/; do (cd "$path"; make; cd ${base}); done
 
 # Create amarisoftenb inst:
 rm -rf inst-amarisoftenb && mkdir inst-amarisoftenb || exit 1
 tar --strip-components=1 -zxf inst-tmp/*/lteenb-linux*.tar.gz -C inst-amarisoftenb/
 if [ "x${BUILD_AMARISOFT_TRX_ZMQ}" = "x1" ]; then
-        cp ${base}/${project_name_srslte}/build/lib/src/phy/rf/libsrslte_rf.so inst-amarisoftenb/
-        cp ${base}/${project_name_zmq}/build/libtrx_zmq-linux-2018-10-18.so inst-amarisoftenb/trx_zmq.so
+        cp ${base}/${project_name_srslte}/build/lib/src/phy/rf/libsrsran_rf.so inst-amarisoftenb/
+        cp ${base}/${project_name_zmq}/build/libtrx_zmq-linux-2021-03-15.so inst-amarisoftenb/trx_zmq.so
         patchelf --set-rpath '$ORIGIN/' inst-amarisoftenb/trx_zmq.so
-        cd inst-amarisoftenb && ln -s libsrslte_rf.so libsrslte_rf.so.0 && cd ..
+        cd inst-amarisoftenb && ln -s libsrsran_rf.so libsrsran_rf.so.0 && cd ..
 fi
-cp ${base}/inst-tmp/trx_uhd-linux*/trx_uhd.so inst-amarisoftenb/
+
+# Untar trx_sdr and copy the libraries to enb.
+tar -xzf inst-tmp/*/trx_sdr*.tar.gz -C inst-tmp/
+cp ${base}/inst-tmp/trx_sdr-linux-2021-03-15/trx_sdr.so inst-amarisoftenb/
+cp ${base}/inst-tmp/trx_sdr-linux-2021-03-15/libsdr.so inst-amarisoftenb/
+cp ${base}/inst-tmp/trx_sdr-linux-2021-03-15/libc_wrapper_sdr.so inst-amarisoftenb/
+
+# Copy the uhd library and create the tarball.
+cp ${base}/inst-tmp/trx_uhd-linux-2021-03-15/trx_uhd.so inst-amarisoftenb/
 this="amarisoftenb.build-${BUILD_NUMBER-$(date +%Y-%m-%d_%H_%M_%S)}"
 tar="${this}.tgz"
 tar -czf "$tar" -C inst-amarisoftenb/ .
@@ -61,14 +67,14 @@ md5sum "$tar" > "${this}.md5"
 # Create amarisoftue inst:
 if [ "x${HAVE_AMARISOFT_LTEUE}" = "x1" ]; then
         rm -rf inst-amarisoftue && mkdir inst-amarisoftue || exit 1
-        tar --strip-components=1 -zxf inst-tmp/*/lteue-linux*.tar.gz -C inst-amarisoftue/
+        tar --strip-components=1 -zxf inst-tmp/*/lteue-linux-2018-10-18.tar.gz -C inst-amarisoftue/
         if [ "x${BUILD_AMARISOFT_TRX_ZMQ}" = "x1" ]; then
-                cp ${base}/${project_name_srslte}/build/lib/src/phy/rf/libsrslte_rf.so inst-amarisoftue/
+                cp ${base}/${project_name_srslte}/build/lib/src/phy/rf/libsrsran_rf.so inst-amarisoftue/
                 cp ${base}/${project_name_zmq}/build/libtrx_zmq-linux-2018-10-18.so inst-amarisoftue/trx_zmq.so
                 patchelf --set-rpath '$ORIGIN/' inst-amarisoftue/trx_zmq.so
-                cd inst-amarisoftue && ln -s libsrslte_rf.so libsrslte_rf.so.0 && cd ..
+                cd inst-amarisoftue && ln -s libsrsran_rf.so libsrsran_rf.so.0 && cd ..
         fi
-        cp ${base}/inst-tmp/trx_uhd-linux*/trx_uhd.so inst-amarisoftue/
+        cp ${base}/inst-tmp/trx_uhd-linux-2018-10-18/trx_uhd.so inst-amarisoftue/
         this="amarisoftue.build-${BUILD_NUMBER-$(date +%Y-%m-%d_%H_%M_%S)}"
         tar="${this}.tgz"
         tar -czf "$tar" -C inst-amarisoftue/ .
@@ -78,8 +84,16 @@ fi
 # Create amarisoftepc inst:
 rm -rf inst-amarisoftepc && mkdir inst-amarisoftepc || exit 1
 tar --strip-components=1 -zxf inst-tmp/*/ltemme-linux*.tar.gz -C inst-amarisoftepc/
+
+# place newer libssl-1.1 and libcrypto-1.1 for older OS distributions inside the EPC folder
+cp inst-tmp/*/libs/libssl* inst-amarisoftepc/
+cp inst-tmp/*/libs/libcrypto* inst-amarisoftepc/
+
 # Copy ltesim_server from UE package if available
 if [ "x${HAVE_AMARISOFT_LTEUE}" = "x1" ]; then
+        # Untar newer lteue version for fresh ltesim_server (2018 uses libssl-1.0.0)
+        rm -rf inst-amarisoftue && mkdir inst-amarisoftue || exit 1
+        tar --strip-components=1 -zxf inst-tmp/*/lteue-linux-2021-03-15.tar.gz -C inst-amarisoftue/
         cp inst-amarisoftue/ltesim_server inst-amarisoftepc/
 fi
 this="amarisoftepc.build-${BUILD_NUMBER-$(date +%Y-%m-%d_%H_%M_%S)}"

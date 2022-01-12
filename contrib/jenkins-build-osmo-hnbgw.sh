b4cacca4537e830a048528d197d97b2e0ad2d06b
Pau Espin Pedrol <pespin@sysmocom.de>
2022-01-12 10:23:32 +0100

    contrib: Convert trial osmo-iuh->osmo-hnbgw
    The real target was not osmo-iuh itself but osmo-hnbgw. Since osmo-hnbgw
    is inits own repository now, let's rename it.
    
    Change-Id: I03c0988db5c5d72bcac71b4515e40a099600ce7c

--- contrib/jenkins-build-osmo-hnbgw.sh
+++ contrib/jenkins-build-osmo-hnbgw.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e -x
 base="$PWD"
-name="osmo-iuh"
+name="osmo-hnbgw"
 . "$(dirname "$0")/jenkins-build-common.sh"
 
 build_repo libosmocore ${SANITIZE_FLAGS} --disable-doxygen
@@ -9,7 +9,8 @@ build_repo libosmo-abis ${SANITIZE_FLAGS} --disable-dahdi
 build_repo libosmo-netif ${SANITIZE_FLAGS} --disable-doxygen
 build_repo libosmo-sccp ${SANITIZE_FLAGS}
 build_repo libasn1c ${SANITIZE_FLAGS}
-
 build_repo osmo-iuh ${SANITIZE_FLAGS}
+
+build_repo osmo-hnbgw ${SANITIZE_FLAGS}
 
 create_bin_tgz "osmo-hnbgw"
